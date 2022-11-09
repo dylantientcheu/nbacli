c75fb249a299fd2f070f62381ba110d548e07342
Dylan Tientcheu <dylantientcheu@gmail.com>
2022-11-10 00:15:29 +0100

    feat: update help tools commands

---
+++ ui/constants/consts.go
@@ -0,0 +1,68 @@
+package constants
+
+import (
+	"nba-cli/nba"
+
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+/* CONSTANTS */
+
+var (
+	// P the current tea program
+	P *tea.Program
+	// Er the entry repository for the tui
+	Gm *nba.Game
+	// Pr the project repository for the tui
+	Sb *nba.ScoreboardRepository
+	// WindowSize store the size of the terminal window
+	WindowSize tea.WindowSizeMsg
+)
+
+/* STYLING */
+
+// DocStyle styling for viewports
+var DocStyle = lipgloss.NewStyle().Margin(0, 2)
+
+// HelpStyle styling for help context menu
+var HelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Render
+
+// ErrStyle provides styling for error messages
+var ErrStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#bd534b")).Render
+
+// AlertStyle provides styling for alert messages
+var AlertStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("62")).Render
+
+type keymap struct {
+	Enter     key.Binding
+	Yesterday key.Binding
+	Tomorrow  key.Binding
+	Back      key.Binding
+	Quit      key.Binding
+}
+
+// Keymap reusable key mappings shared across models
+var Keymap = keymap{
+	Enter: key.NewBinding(
+		key.WithKeys("enter"),
+		key.WithHelp("enter", "select"),
+	),
+	Yesterday: key.NewBinding(
+		key.WithKeys("i"),
+		key.WithHelp("i", "yesterday"),
+	),
+	Tomorrow: key.NewBinding(
+		key.WithKeys("o"),
+		key.WithHelp("o", "tomorrow"),
+	),
+	Back: key.NewBinding(
+		key.WithKeys("esc"),
+		key.WithHelp("esc", "back"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("ctrl+c", "q"),
+		key.WithHelp("ctrl+c/q", "quit"),
+	),
+}
---
+++ ui/scoreboards.go
@@ -0,0 +1,116 @@
+package ui
+
+import (
+	"fmt"
+	"nba-cli/nba"
+	"nba-cli/ui/constants"
+	"time"
+
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type mode int
+
+// SelectMsg the message to change the view to the selected entry
+type SelectMsg struct {
+	ActiveScorebardID uint
+}
+
+const (
+	nav mode = iota
+	edit
+)
+
+type Model struct {
+	mode     mode
+	list     list.Model
+	quitting bool
+	gameview bool
+}
+
+func InitScoreboard(date time.Time) tea.Model {
+	items := newScoreboardList(constants.Sb, date)
+	m := Model{mode: nav, list: list.NewModel(items, list.NewDefaultDelegate(), 8, 8)}
+	if constants.WindowSize.Height != 0 {
+		top, right, bottom, left := constants.DocStyle.GetMargin()
+		m.list.SetSize(constants.WindowSize.Width-left-right, constants.WindowSize.Height-top-bottom-1)
+	}
+	m.list.Title = "NBA Games"
+	m.list.AdditionalShortHelpKeys = func() []key.Binding {
+		return []key.Binding{
+			constants.Keymap.Tomorrow,
+			constants.Keymap.Yesterday,
+			constants.Keymap.Back,
+		}
+	}
+	return m
+}
+
+func newScoreboardList(scbrd *nba.ScoreboardRepository, date time.Time) []list.Item {
+	games := scbrd.GetGames(date)
+	return gamesToItems(games)
+}
+
+// Init run any intial IO on program start
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+// Update handle IO and commands
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	var cmds []tea.Cmd
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		constants.WindowSize = msg
+		top, right, bottom, left := constants.DocStyle.GetMargin()
+		m.list.SetSize(msg.Width-left-right, msg.Height-top-bottom-1)
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, constants.Keymap.Yesterday):
+			return m, tea.Quit
+		case key.Matches(msg, constants.Keymap.Tomorrow):
+			return m, tea.Quit
+		case key.Matches(msg, constants.Keymap.Quit):
+			m.quitting = true
+			return m, tea.Quit
+		case key.Matches(msg, constants.Keymap.Enter):
+			m.gameview = true
+			activeGame := m.list.SelectedItem().(nba.Game)
+			fmt.Printf("%#v", activeGame.GameId)
+			return m, tea.Quit // enter the game id
+			// entry := InitEntry(constants.Er, activeProject.ID, constants.P)
+			// return entry.Update(constants.WindowSize)
+		default:
+			m.list, cmd = m.list.Update(msg)
+		}
+		cmds = append(cmds, cmd)
+	}
+	return m, tea.Batch(cmds...)
+}
+
+// View return the text UI to be output to the terminal
+func (m Model) View() string {
+	if m.quitting {
+		return ""
+	}
+	return constants.DocStyle.Render(m.list.View() + "\n")
+}
+
+// TODO: use generics
+// gamesToItems convert []model.Project to []list.Item
+func gamesToItems(games []nba.Game) []list.Item {
+	items := make([]list.Item, len(games))
+	for i, proj := range games {
+		items[i] = list.Item(proj)
+	}
+	return items
+}
+
+func (m Model) getActiveGameID() string {
+	items := m.list.Items()
+	activeItem := items[m.list.Index()]
+	return activeItem.(nba.Game).GameId
+}
