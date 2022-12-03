bac865886564f3b049f6f0616968304eefba7ddf
Dylan Tientcheu <dylantientcheu@gmail.com>
2022-12-03 17:26:00 +0100

    bug: remove single game command

--- cmd/game.go
+++ cmd/game.go
@@ -46,26 +46,11 @@ var gameCmd = &cobra.Command{
 	},
 }
 
-// todo: get single game search or id
-var gameIdCmd = &cobra.Command{
-	Use:   "specific",
-	Short: "Get a single nba game by ID",
-	Run: func(cmd *cobra.Command, args []string) {
-		bxScrSummary := nba.BoxScoreRepository{}
-		bxScrSummary.GetSingleGameStats(gameID)
-
-		// TODO: start TUI for single game
-	},
-}
-
 func init() {
 	rootCmd.AddCommand(gameCmd)
-	rootCmd.AddCommand(gameIdCmd)
 	rootCmd.PersistentFlags().StringVarP(&date, "date", "d", "", "Date to get the schedule for (YYYYMMDD)")
 	rootCmd.PersistentFlags().BoolVarP(&hasYesterday, "yesterday", "y", false, "Get yesterday's games")
 	rootCmd.PersistentFlags().BoolVarP(&hasTomorrow, "tomorrow", "t", false, "Get tomorrow's games")
-
-	// rootCmd.PersistentFlags().StringVarP(&gameID, "game", "g", "", "Get a single game by ID")
 
 	rootCmd.MarkFlagsMutuallyExclusive("yesterday", "tomorrow", "date")
 }
